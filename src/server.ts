0321e279fba33cd19acbf4292e831eef50c9a7f8
Linda-Ikechukwu <lindaedwin96@gmail.com>
2020-06-23 12:31:53 +0100

    Added restful endpoint for image manipulation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+//import { try } from 'bluebird';
 
 (async () => {
 
@@ -17,7 +19,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -29,7 +30,34 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  //! END @TODO1
+  app.get('/filteredimage', async (req: Request, res: Response) => {
+    let {image_url} = req.query;
+
+    if ( !image_url ) {
+      return res.status(400).send(`Bad Request: image url query is required`);
+    }
+    
+    try{
+      const filteredImage = await filterImageFromURL(image_url);
+      res.status(200).sendFile(filteredImage, async function (err) {
+        if (err) {
+          console.log(err);
+        }else {
+          try {
+            await deleteLocalFiles([filteredImage]); 
+          }catch(e) {
+            console.log("error removing image", e); 
+          }
+        }
+      });
+    
+    }catch (error) {
+      console.error(error);
+    }
+    
+  })
+
+  
   
   // Root Endpoint
   // Displays a simple message to the user
